49ca8031413f26990f8bca0ebdeb74ebf54826c4
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-06-17 18:20:59 -0700

    adding input options

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,24 @@ class ProductsController < ApplicationController
  
   def index
     #get all the products from the db
-    product = Product2.all
+    product = Product.all
     render json: product.as_json
   end
 
   def show
     the_id = params[:id]
-    product = Product2.find_by(id: the_id)
+    product = Product.find_by(id: the_id)
     render json: recipe.as_json 
+  end
+
+  def create
+    product = Product.new(
+      name: params[:input_options], 
+      price: params[:input_options], 
+      description: params[:input_options], 
+      img_url: params[:input_options]
+    )
+    product.save
+    render json: product.as_json
   end
 end
