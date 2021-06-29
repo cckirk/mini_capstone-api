ce414d0592565a4690ed17faa575ac975bf50053
Christopher Kirkwood <christopherkirkwood@Christophers-MacBook-Air.local>
2021-06-29 19:25:55 -0400

    adding authorization

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,15 +3,16 @@ class OrdersController < ApplicationController
     product = Product.find_by(id: params[:product_id])
     calculated_subtotal = params[:quantity].to_i * product.price
     calculated_tax = calculated_subtotal * 0.09
-
+    calculated_total = calculated_subtotal + calculated_tax
     order = Order.new(
       user_id: current_user.id, 
       product_id: params[:product_id] || order.price, 
       quantity: params[:quantity] || order.quantity,
       subtotal: calculated_subtotal,
       tax: calculated_tax,
-      total: params[:total] || order.total
+      total: calculated_total
     )
+
     order.save
     if order.save
       render json: order.as_json
@@ -21,14 +22,16 @@ class OrdersController < ApplicationController
   end
 
   def show
-
-
     order = Order.find_by(id: params[:id], user_id: current_user.id)
     render json: order.as_json
   end
 
   def index
-    orders = Order.where(user_id: current_user.id)
-    render json: orders.as_json
+    if current_user
+      orders = Order.where(user_id: current_user.id)
+      render json: orders.as_json
+    else
+      render json: [], status: :unauthorized
+    end
   end
 end
